af91499860ba5efc42aabeea3abda1596434581a
aakulgina <51416227+aakulgina@users.noreply.github.com>
2020-03-13 21:26:32 +0400

    Update printTriangle.js

--- 4/printTriangle.js
+++ 4/printTriangle.js
@@ -2,7 +2,7 @@ function printTriangle() {
     var i = 0
     var size = 5
     while (i < size+1) {
-    console.log(" ".repeat(size-i), " #".repeat(i));
+    console.log(" ".repeat(size-i), " #".repeat(i))
     i++
     }
 }
