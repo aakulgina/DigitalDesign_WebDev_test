42afd43fa064004e4c73ebd0ed93e00dd7c3a926
aakulgina <51416227+aakulgina@users.noreply.github.com>
2020-03-15 21:06:13 +0400

    Correct Task 5

--- 5/array.js
+++ 5/array.js
@@ -1,26 +1,26 @@
 var arr = new Array(5)
 
 function fillArray(array) {
-	for (var i = 0; i < array.length; i++) {
-		array[i] = Math.floor(Math.random() * (100 - (-100) + 1) + (-100))
-	}
-	return array
+    for (var i = 0; i < array.length; i++) {
+        array[i] = Math.floor(Math.random() * (100 - (-100) + 1) + (-100))
+    }
+    return array
 }
 
 function replaceFromArray(array) {
-	for (var i = 0; i < array.length; i++) {
-		if (array[i] < 0) array[i] = 0
-	}
-	return array
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] < 0) array[i] = 0
+    }
+    return array
 }
 
 function addToArray(array) {
-	var k = 0
-	for (var i = 0; i < array.length; i++) {
-		if (array[i] == 0) k++
-		if (k != 2) continue
-	    array.splice(i+1, 0, -1)
-		k = 0
-	}
-	return array
+    var k = 0
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] == 0) k++
+        if (k != 2) continue
+        array.splice(i+1, 0, -1)
+        k = 0
+    }
+    return array
 }
