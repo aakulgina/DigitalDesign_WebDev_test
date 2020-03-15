e0d2265e2e319affebdf7fdce4fd8471262d73d4
aakulgina <51416227+aakulgina@users.noreply.github.com>
2020-03-15 16:56:12 +0400

    Update array.js

--- 5/array.js
+++ 5/array.js
@@ -1,30 +1,22 @@
 var arr = new Array(5)
 
 function fillArray() {
-	var i = 0
-	var max = 100
-	var min = -100
-	while (i < arr.length) {
-		arr[i] = Math.floor(Math.random() * (max - min + 1) + min)
-		i ++
+	for (var i = 0; i < arr.length; i++) {
+		arr[i] = Math.floor(Math.random() * (100 - (-100) + 1) + (-100))
 	}
 }
 
 function replaceFromArray() {
-	var i = 0
-	while (i < arr.length) {
-		if (arr[i] < 0) {
-			arr[i] = 0
-		}
-		i++
+	for (var i = 0; i < arr.length; i++) {
+		if (arr[i] < 0) arr[i] = 0
 	}
 }
 
 function addToArray() {
 	var indx = new Array(0)
-	var i = 0
 	var k = 0
-	while (i < arr.length) {
+	
+	for (var i = 0; i < arr.length; i++) {
 		if (arr[i] == 0) {
 			k++
 			if (k == 2) {
@@ -32,11 +24,9 @@ function addToArray() {
 				k = 0
 			}
 		}
-		i++
 	}
-	var i = 0
-	while (i < indx.length) {
-		arr.splice(indx[i]+i, 0, -1)
-		i++
-	}
+		
+		for (var i = 0; i < indx.length; i++) {
+			arr.splice(indx[i]+i, 0, -1)
+		}
 }
