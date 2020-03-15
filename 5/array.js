d67f4b97c2de2db9e60c665f1b171b72f265fd26
aakulgina <51416227+aakulgina@users.noreply.github.com>
2020-03-15 20:50:51 +0400

    Correct Task 5

--- 5/array.js
+++ 5/array.js
@@ -1,32 +1,26 @@
 var arr = new Array(5)
 
-function fillArray() {
-	for (var i = 0; i < arr.length; i++) {
-		arr[i] = Math.floor(Math.random() * (100 - (-100) + 1) + (-100))
+function fillArray(array) {
+	for (var i = 0; i < array.length; i++) {
+		array[i] = Math.floor(Math.random() * (100 - (-100) + 1) + (-100))
 	}
+	return array
 }
 
-function replaceFromArray() {
-	for (var i = 0; i < arr.length; i++) {
-		if (arr[i] < 0) arr[i] = 0
+function replaceFromArray(array) {
+	for (var i = 0; i < array.length; i++) {
+		if (array[i] < 0) array[i] = 0
 	}
+	return array
 }
 
-function addToArray() {
-	var indx = new Array(0)
+function addToArray(array) {
 	var k = 0
-	
-	for (var i = 0; i < arr.length; i++) {
-		if (arr[i] == 0) {
-			k++
-			if (k == 2) {
-				indx.push(i+1)
-				k = 0
-			}
-		}
+	for (var i = 0; i < array.length; i++) {
+		if (array[i] == 0) k++
+		if (k != 2) continue
+	    array.splice(i+1, 0, -1)
+		k = 0
 	}
-		
-		for (var i = 0; i < indx.length; i++) {
-			arr.splice(indx[i]+i, 0, -1)
-		}
+	return array
 }
